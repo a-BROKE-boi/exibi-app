d768e8413789be2bf59aafa40df5968e002a5752
Caio Whitmore <caiowhitmore@outlook.com>
2024-12-17 17:11:27 +0000

    removed babble error

--- exibi-app/app/gallery/page.js
+++ exibi-app/app/gallery/page.js
@@ -1,4 +1,6 @@
-import React from 'react';
+'use client'
+
+import React, { useState, useEffect } from 'react';
 import Image from 'next/image';
 
 
@@ -8,7 +10,7 @@ export default  function gallery () {
         <div className="mx-auto max-w-2xl py-16 px-4 sm:py-24 sm:px-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
     <div className="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
       <BlurImage />
-      {/* Add more BlurImage components here */}
+     
     </div>
   </div>
   </>
@@ -16,21 +18,39 @@ export default  function gallery () {
     
 }
 
-function BlurImage() {
 
+
+
+function BlurImage({ imageUrl }) {
+
+  async function images() {
+    const data = await fetch('https://collectionapi.metmuseum.org/public/collection/v1/objects/[objectID]')
+    const musiumAPI = await data.json()
     return (
-      <a href="#" className="group">
-        <div className="aspect-w-1 aspect-h-1 xl:aspect-w-7 xl:aspect-h-8 w-full overflow-hidden rounded-lg bg-gray-200">
-          <Image
-            alt=""
-            src="https://images.metmuseum.org/CRDImages/ep/web-large/DT1567.jpg"
-            layout="fill"
-            objectFit='cover'
-            className="group-hover:opacity-75"
-          />
-        </div>
-        <h3 className="mt-4 text-sm text-gray-700">place</h3>
-        <p className="mt-1 text-lg font-medium text-gray-900">holder</p>
-      </a>
+      
+      <ul>
+        {musiumAPI.map((musiumAPI) => (
+          <a href="#" className="group">
+            <li key={musiumAPI.id}>
+          <div className="aspect-w-1 aspect-h-1 xl:aspect-w-7 xl:aspect-h-8 w-full overflow-hidden rounded-lg bg-gray-200">
+            <Image
+              alt={musiumAPI.title}
+              src={ musiumAPI.imageUrl }
+              layout="fill"
+              objectFit='cover'
+              className="group-hover:opacity-75"
+            />
+          </div>
+          <h3 className="mt-4 text-sm text-gray-700">place</h3>
+          <p className="mt-1 text-lg font-medium text-gray-900">holder</p>
+          </li>
+        </a>
+          
+        ))}
+      </ul>
     )
   }
+
+  }
+
+  
