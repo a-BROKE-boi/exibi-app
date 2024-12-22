db5465dd019f7d9f849ada13e092b15ba4cfa094
Caio Whitmore <caiowhitmore@outlook.com>
2024-12-22 11:56:59 +0000

    resolved errors beguin resolving image itteration

--- exibi-app/app/gallery/page.js
+++ exibi-app/app/gallery/page.js
@@ -1,74 +1,59 @@
 
+import Image from "next/image";
 export default async function gallery () {
-  for (let i = 1; i < 3; i++) { 
-  const data = await fetch(`https://collectionapi.metmuseum.org/public/collection/v1/objects/${i}`)
-  const posts = await data.json()
-
-    return (<>
-    <p>{JSON.stringify(posts)}</p>
-    
-   
   
+  const data = await fetch(`https://collectionapi.metmuseum.org/public/collection/v1/objects`)
+  const musiumJSON = await data.json()
+
+  return (
+    <>
+    <p>total in met db: {musiumJSON.total}</p>
+    <FetchMetMusiumArt/>
+    
   </>
-    )
+  );
     
+
 }
-}
-// export default async function Page() {
-//   const data = await fetch('https://api.vercel.app/blog')
-//   const posts = await data.json()
-//   return (
-//     <ul>
-//       {posts.map((post) => (
-//         <li key={post.id}>{post.title}</li>
-//       ))}
-//     </ul>
-//   )
+// async function FetchMetMusiumArt() {
+//   let ids = [1,2,3,4,5,6,7,8,9]
+
+//   try {
+//     const promises = ids.map(id => fetch(`https://collectionapi.metmuseum.org/public/collection/v1/objects/${id}`));
+//     const responses = await Promise.all(promises);
+//     const artJSON = await Promise.all(responses.map(response => response.json()));
+//     return artJSON;
+//   } catch (error) {
+//     return (<>Error fetching museum data:{error}</>)
+//   }
 // }
-// iterate over 10 at a time we could only iterate over the fetch function 10 calls at a time.
 
-
-// trying to figure out the json
-
-
-
-
-
-//   async function FetchMetMusium() {
-//     const artId = 1
-//     const data = await fetch(`https://collectionapi.metmuseum.org/public/collection/v1/objects/${artId}`)
-//     const musiumJSON = await data.json()
-//     Object.keys(musiumJSON).forEach(key => {      
+async function FetchMetMusiumArt() {
   
+  for (let i = 1; i < 10; i++) {
+      const data = await fetch(`https://collectionapi.metmuseum.org/public/collection/v1/objects/${i}`)
+      const artJSON = await data.json()
+      if (artJSON.isPublicDomain === true) {
+        return (
+          <a href="#" className="group">
+            <ul key={artJSON.objectID}>
+              <div className="aspect-w-1 aspect-h-1 xl:aspect-w-7 xl:aspect-h-8 w-full overflow-hidden rounded-lg bg-gray-200">
+            <Image
+              alt={ artJSON.title }
+              src={ artJSON.primaryImageSmall }
+              layout="fill"
+              objectFit='cover'
+              className="group-hover:opacity-75"
+            />
+          </div>
+          <h3 className="mt-4 text-sm text-gray-700">{ artJSON.objectName	}</h3>
+          <p className="mt-1 text-lg font-medium text-gray-900">{ artJSON.title }</p>
+          </ul>
+        </a>
+        )
+      }
+        
+      }
+    }
   
-//     return (
-//       <>
-//       <p>
-//       `${key}: ${obj[key]}`
-//       </p>
-//       </>
-
-
-//       // <ul>
-//       //   {musiumJSON.map((Art) => (
-//       //     <a href="#" className="group">
-//       //       <li key={Art.objectID}>
-//       //     <div className="aspect-w-1 aspect-h-1 xl:aspect-w-7 xl:aspect-h-8 w-full overflow-hidden rounded-lg bg-gray-200">
-//       //       <Image
-//       //         alt={ Art.title }
-//       //         src={ Art.primaryImageSmall }
-//       //         layout="fill"
-//       //         objectFit='cover'
-//       //         className="group-hover:opacity-75"
-//       //       />
-//       //     </div>
-//       //     <h3 className="mt-4 text-sm text-gray-700">{ Art.objectName	}</h3>
-//       //     <p className="mt-1 text-lg font-medium text-gray-900">{ Art.title }</p>
-//       //     </li>
-//       //   </a>
-//       //   ))}
-//       // </ul>
-//     )
-//   })
-// }
   
